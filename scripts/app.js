12c25ad82b6f3851b167720844752738625f5da8
MohamedHassan499 <hamobos499@gmail.com>
2023-04-16 14:48:37 +0200

    Changed tons of things

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,9 @@ import { jobsNames, jobJoiningDate, jobDescription } from "./data/jobs.js";
 const addCompanyPosition = (job) => {
   const companyH2 = document.getElementById("company");
   companyH2.innerHTML = `Software Engineer <span class="company-name">@${
-    job.id === "tech-hive"
+    job === "tech-hive"
       ? "Tech Hive"
-      : job.id === "shoofdoctor"
+      : job === "shoofdoctor"
       ? "Shoofdoctor"
       : "ITI"
   }</span>`;
@@ -34,28 +34,48 @@ jobs.forEach((job) => {
     job.classList.add("clicked");
     job.style.borderLeft = "2px solid #64ffda";
     if (job.id === "tech-hive") {
-      console.log("tech-hive");
       jobs[0].style.borderLeft = "2px solid #233554";
       jobs[1].style.borderLeft = "2px solid #233554";
     }
     if (job.id === "shoofdoctor") {
-      console.log("shoofdoctor");
       jobs[0].style.borderLeft = "2px solid #233554";
       jobs[2].style.borderLeft = "2px solid #233554";
     }
     if (job.id === "iti") {
-      console.log("iti");
       jobs[1].style.borderLeft = "2px solid #233554";
       jobs[2].style.borderLeft = "2px solid #233554";
     }
-    addCompanyPosition(job);
+    addCompanyPosition(job.id);
     addCompanyPeriodWork(job.id);
     addCompanyDescription(job.id);
   });
 });
 
+const anim = anime.timeline({
+  loop: true,
+  direction: "alternate",
+});
+
+console.log(anim);
+
+anim
+  .add({
+    targets: "#hexagon path",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutQuart",
+    duration: 2000,
+    delay: function (el, i) {
+      return i * 250;
+    },
+  })
+  .add({
+    targets: "#hexagon #F",
+    duration: 1000,
+    opacity: 1,
+    easing: "easeInOutQuart",
+  });
 setTimeout(() => {
   document.getElementsByClassName("loader")[0].style.display = "none";
   document.getElementsByTagName("header")[0].style.display = "block";
   document.getElementsByTagName("main")[0].style.display = "block";
-}, 2000)();
+}, 4500)();
